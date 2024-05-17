9d2358ef5df1eeb65596ba3f29eb0c8d3b96f479
szittja <szittja21@gmail.com>
2024-05-17 21:02:21 +0200

    Foglalas és Szalloda osztály létrehozása

--- beadando.py
+++ beadando.py
@@ -23,3 +23,47 @@ class KetagyasSzoba(Szoba):
 
     def __str__(self):
         return f"Kétágyas szoba {self.szobaszam}, Ár: {self.ar} Ft/éj"
+
+
+class Foglalas:
+    def __init__(self, szoba, datum):
+        self.szoba = szoba
+        self.datum = datum
+
+
+class Szalloda:
+    def __init__(self, nev):
+        self.nev = nev
+        self.szobak = []
+        self.foglalasok = []
+
+    def szoba_hozzaadas(self, szoba):
+        self.szobak.append(szoba)
+
+    def foglal(self, szobaszam, datum):
+        for foglal in self.foglalasok:
+            if foglal.szoba.szobaszam == szobaszam and foglal.datum == datum:
+                print("A szoba már foglalt ezen a napon, válasszon másik szobát vagy dátumot!")
+                return None
+        for szoba in self.szobak:
+            if szoba.szobaszam == szobaszam:
+                self.foglalasok.append(Foglalas(szoba, datum))
+                return szoba.ar
+        return None
+
+    def lemond(self, szobaszam, datum):
+        for foglal in self.foglalasok:
+            if foglal.szoba.szobaszam == szobaszam and foglal.datum == datum:
+                self.foglalasok.remove(foglal)
+                return True
+        return False
+
+    def foglalas_lista(self):
+        print("Foglalások listája:")
+        for foglal in self.foglalasok:
+            print(f"Szoba: {foglal.szoba.szobaszam}, Dátum: {foglal.datum}")
+
+    def szobak_listaja(self):
+        print("Szobák listája:")
+        for szoba in self.szobak:
+            print(szoba)
