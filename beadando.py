583095c86fb26cd596c76247e3d05597083bf725
szittja <szittja21@gmail.com>
2024-05-17 21:10:27 +0200

    Felhasználó felület létrehozása

--- beadando.py
+++ beadando.py
@@ -82,3 +82,70 @@ for i in range(5):
         random_nap = mainap + timedelta(random.randint(0, 20))
         if hotel.foglal(random_szoba.szobaszam, random_nap):
             break
+
+print(f"Üdvözöljük a {hotel.nev} foglalási rendszerében!")
+
+while True:
+    print("\nVálasszon az alábbi opciók közül:")
+    print("1. Szobák listázása")
+    print("2. Foglalások listázása")
+    print("3. Szoba foglalás")
+    print("4. Foglalás lemondása")
+    print("0. Kilépés a foglaló rendszerből\n")
+    valasztas = input("Kérem adja meg a kívánt opció számát: ")
+
+    if valasztas == "1":
+        hotel.szobak_listaja()
+
+    elif valasztas == "2":
+        hotel.foglalas_lista()
+
+    elif valasztas == "3":
+        while True:
+            szobaszam = input("Adjon meg egy szobaszámot: ")
+            szoba = next((sz for sz in hotel.szobak if sz.szobaszam == szobaszam), None)
+            if szoba is None:
+                print("Nincs ilyen szoba a szállodában.")
+                hotel.szobak_listaja()
+                continue
+            while True:
+                datum = input("Adja meg a dátumot (éééé-hh-nn formátumban): ")
+                try:
+                    datum = datetime.strptime(datum, "%Y-%m-%d").date()
+                    if datum < mainap:
+                        raise ValueError("Kérjük az aktuális vagy jövőbeli napot válasszon a foglalásra!")
+                    break
+                except ValueError as e:
+                    print(str(e))
+            foglal = hotel.foglal(szobaszam, datum)
+            if foglal:
+                print(f"A foglalás sikeres! Az ár: {foglal} Ft")
+                break
+
+    elif valasztas == "4":
+        while True:
+            szobaszam = input("Adjon meg egy szobaszámot: ")
+            if not any(foglal.szoba.szobaszam == szobaszam for foglal in hotel.foglalasok):
+                print("Nincs ilyen szobára foglalás a szállodában.")
+                continue
+            while True:
+                datum = input("Adja meg a lemondandó foglalás dátumát (éééé-hh-nn formátumban): ")
+                try:
+                    datum = datetime.strptime(datum, "%Y-%m-%d").date()
+                    if datum < mainap:
+                        print("Csak jövőbeni foglalás mondható le.")
+                    else:
+                        break
+                except ValueError:
+                    print("Hibás dátum formátum!")
+            if hotel.lemond(szobaszam, datum):
+                print("A foglalás sikeresen lemondva.")
+                break
+            else:
+                print("Nincs ilyen foglalás.")
+
+    elif valasztas == "0":
+        print("Köszönjük, hogy szállodánkat választotta. Viszont látásra!")
+        break
+    else:
+        print("Hibás választás, kérjük próbálja újra.")
